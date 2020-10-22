b3bd01033e7bcb5d1df66d29e607448bf831d52d
Beimar Colque <beimarcolque987@gmail.com>
2020-10-22 16:40:44 -0400

    version beimar

--- src/proyectoreceta/ProyectoReceta.java
+++ src/proyectoreceta/ProyectoReceta.java
@@ -15,6 +15,7 @@ public class ProyectoReceta {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.print("hola");
         // 
     }
     
