a525bb8bc5c3ed1e94c3831702db304b93ba4f4a
TATIANA <TATIANA@192.168.139.1>
2020-10-22 15:55:20 -0400

    Hola Tatiana

--- src/proyectoreceta/ProyectoReceta.java
+++ src/proyectoreceta/ProyectoReceta.java
@@ -14,8 +14,9 @@ public class ProyectoReceta {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // 
+    public static void main(String[] args)
+    {
+        System.out.println("Hola Tatiana");
     }
     
 }
